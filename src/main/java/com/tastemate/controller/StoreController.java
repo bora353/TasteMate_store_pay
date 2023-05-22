35662df5d6383e9f7d634193c25315536f061840
bora353 <117142490+bora353@users.noreply.github.com>
2023-05-22 16:51:04 +0900

    5/22

--- src/main/java/com/tastemate/controller/StoreController.java
+++ src/main/java/com/tastemate/controller/StoreController.java
@@ -7,8 +7,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -37,6 +41,36 @@ public class StoreController {
 
         model.addAttribute("storeVO", storeVO);
 
+    }
+
+    @GetMapping("/register")
+    public void register(){
+
+    }
+
+    @PostMapping("/register")
+    public String registerStoreVO(StoreVO storeVO, MultipartFile[] uploadFile, Model model){
+
+
+        String uploadFolder = "C:\\upload";
+
+        for (MultipartFile multipartFile : uploadFile) {
+
+            log.info("===========================");
+            log.info("upload file name : " + multipartFile.getOriginalFilename());
+            log.info("upload file size : " + multipartFile.getSize());
+
+            File saveFile = new File(uploadFolder, multipartFile.getOriginalFilename());
+
+            try {
+                multipartFile.transferTo(saveFile);
+            }catch (Exception e) {
+                log.error(e.getMessage());
+            }
+        }
+
+
+        return "redirect:/store/list";
     }
 
 }
--- target/classes/sql/store.sql
+++ target/classes/sql/store.sql
@@ -43,7 +43,8 @@ CREATE TABLE STORE
     , STORE_NAME VARCHAR2(100 BYTE) NOT NULL
     , CATEGORY1 VARCHAR2(50 BYTE) NOT NULL
     , STORE_ADDRESS VARCHAR2(200 BYTE) NOT NULL
-    , STORE_API VARCHAR2(200 BYTE)
+    , STORE_lati NUMBER
+    , STORE_longi NUMBER
     , PHONE_NUMBER VARCHAR2(100 BYTE)
     , STORE_COUNT NUMBER DEFAULT 0
     , FILENAME VARCHAR2(200 BYTE)
@@ -58,38 +59,38 @@ CREATE TABLE STORE
 CREATE SEQUENCE store_seq;
 
 INSERT INTO STORE (STORE_IDX, USER_IDX, STORE_NAME, CATEGORY1,
-                   STORE_ADDRESS, STORE_API, PHONE_NUMBER, STORE_COUNT,
+                   STORE_ADDRESS, STORE_LATI, STORE_LONGI, PHONE_NUMBER, STORE_COUNT,
                    FILENAME, ORI_FILENAME)
 VALUES (store_seq.NEXTVAL, 1, '농민백암순대', '한식',
-        '서울 강남구 역삼로3길 20-4', '...', '02-501-2772', 0,
+        '서울 강남구 역삼로3길 20-4', 37.49490175032281, 127.03149331806011, '02-501-2772', 0,
         'tastemate.jpg', 'tastemate.jpg');
 
 INSERT INTO STORE (STORE_IDX, USER_IDX, STORE_NAME, CATEGORY1,
-                   STORE_ADDRESS, STORE_API, PHONE_NUMBER, STORE_COUNT,
+                   STORE_ADDRESS, STORE_LATI, STORE_LONGI, PHONE_NUMBER, STORE_COUNT,
                    FILENAME, ORI_FILENAME)
 VALUES (store_seq.NEXTVAL, 1, '청목', '한식',
-        '서울 강남구 테헤란로 124 삼원타워', '...', '02-557-5534', 0,
+        '서울 강남구 테헤란로 124 삼원타워', 37.49877828305107,127.0316730592617, '02-557-5534', 0,
         'tastemate.jpg', 'tastemate.jpg');
 
 INSERT INTO STORE (STORE_IDX, USER_IDX, STORE_NAME, CATEGORY1,
-                   STORE_ADDRESS, STORE_API, PHONE_NUMBER, STORE_COUNT,
+                   STORE_ADDRESS, STORE_LATI, STORE_LONGI, PHONE_NUMBER, STORE_COUNT,
                    FILENAME, ORI_FILENAME)
 VALUES (store_seq.NEXTVAL, 1, '슈퍼심플', '양식',
-        '서울 강남구 테헤란로8길 28 1층', '...', '0507-1352-4838', 0,
+        '서울 강남구 테헤란로8길 28 1층', 37.49707535532538,  127.03176846245647, '0507-1352-4838', 0,
         'tastemate.jpg', 'tastemate.jpg');
 
 INSERT INTO STORE (STORE_IDX, USER_IDX, STORE_NAME, CATEGORY1,
-                   STORE_ADDRESS, STORE_API, PHONE_NUMBER, STORE_COUNT,
+                   STORE_ADDRESS, STORE_LATI, STORE_LONGI, PHONE_NUMBER, STORE_COUNT,
                    FILENAME, ORI_FILENAME)
 VALUES (store_seq.NEXTVAL, 1, '먼키 강남역점', '푸드코트',
-        '서울 강남구 테헤란로 123 여삼빌딩 B1층', '...', '0507-1477-7117', 0,
+        '서울 강남구 테헤란로 123 여삼빌딩 B1층', 37.49948341606353, 127.0313143000319, '0507-1477-7117', 0,
         'tastemate.jpg', 'tastemate.jpg');
 
 INSERT INTO STORE (STORE_IDX, USER_IDX, STORE_NAME, CATEGORY1,
-                   STORE_ADDRESS, STORE_API, PHONE_NUMBER, STORE_COUNT,
+                   STORE_ADDRESS, STORE_LATI, STORE_LONGI, PHONE_NUMBER, STORE_COUNT,
                    FILENAME, ORI_FILENAME)
 VALUES (store_seq.NEXTVAL, 1, '오미라식당', '퓨전',
-        '서울 강남구 테헤란로 124 삼원타워 지하1층', '...', '0507-1384-0510', 0,
+        '서울 강남구 테헤란로 124 삼원타워 지하1층', 37.49877828305107,127.0316730592617, '0507-1384-0510', 0,
         'tastemate.jpg', 'tastemate.jpg');
 
 COMMIT;
